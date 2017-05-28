8715557b7357adb1d419046ce3ae92d7d7d8630c
LeeSangHoon <dl57934@naver.com>
2017-05-29 08:30:39 +0900

    드론 날리기 성공

--- sketch_may29a/sketch_may29a.ino
+++ sketch_may29a/sketch_may29a.ino
@@ -3,21 +3,52 @@ int MPU = 0x68;
 float accelX, accelY, accelZ;
 float gyroX, gyroY, gyroZ;
 float dT;
-float roll_prev_angle=0.0, roll_kp=1, roll_ki=0, roll_kd=0, roll_iterm;
-float roll_output, pitch_output, yaw_output;
-float pitch_prev_angle=0.0, pitch_kp=1, pitch_ki=0, pitch_kd=0, pitch_iterm;
-float  yaw_prev_angle=0.0, yaw_kp=1, yaw_ki=0, yaw_kd=0, yaw_iterm;
+//float roll_target_angle=0.0, roll_kp=1, roll_ki=0, roll_kd=0, roll_iterm;
+
+//float pitch_prev_angle=0.0, pitch_kp=1, pitch_ki=0, pitch_kd=0, pitch_iterm;
+//float  yaw_prev_angle=0.0, yaw_kp=1, yaw_ki=0, yaw_kd=0, yaw_iterm;
 float gyro_x, gyro_y, gyro_z;
 float gyro_xz, gyro_yz;
-unsigned long Tnow, Tprev;
+float roll_target_angle = 0.0;
+float roll_angle_in;
+float roll_rate_in;
+float roll_stabilize_kp = 1;
+float roll_stabilize_ki = 0;
+float roll_rate_kp = 1;
+float roll_rate_ki = 0;
+float roll_stabilize_iterm;
+float roll_rate_itrem;
+float roll_output;
+
+float pitch_target_angle = 0.0;
+float pitch_angle_in;
+float pitch_rate_in;
+float pitch_stabilize_kp = 1;
+float pitch_stabilize_ki = 0;
+float pitch_rate_kp = 1;
+float pitch_rate_ki = 0;
+float pitch_stabilize_iterm;
+float pitch_rate_itrem;
+float pitch_output;
+
+float yaw_target_angle = 0.0;
+float yaw_angle_in;
+float yaw_rate_in;
+float yaw_stabilize_kp = 1;
+float yaw_stabilize_ki = 0;
+float yaw_rate_kp = 1;
+float yaw_rate_ki = 0;
+float yaw_stabilize_iterm;
+float yaw_rate_itrem;
+float yaw_output;
 enum
 {
-  head1,head2,head3,
-  sizex,type,roll,pitch,yaw,thro,aux,crc
+  head1, head2, head3,
+  sizex, type, roll, pitch, yaw, thro, aux, crc
 };
 void setup()
 {
-  
+
   Serial.begin(115200);
   Serial1.begin(115200);
   SetI2c();
@@ -35,15 +66,16 @@ void loop()
   calAccel();
   calGyro();
   complementary();
-  calcYPTtoStdPID();
+  //calcYPTtoStdPID();
+  calcYPRtoDualPID();
   calcMotorSpeed();
   checkMspPacket();
   InitSpeed();
   updateSpeed();
-  static int cnt;
+  /*static int cnt;
   cnt++;
   if (cnt % 2 == 0)
-    SendDataToProcessing();
+  SendDataToProcessing();*/
   /* add main program code here */
 
 }
@@ -52,14 +84,14 @@ void getAngle()
   Wire.beginTransmission(MPU);
   Wire.write(0x3B);
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU, 6,true);
+  Wire.requestFrom(MPU, 6, true);
   accelX = Wire.read() << 8 | Wire.read();
   accelY = Wire.read() << 8 | Wire.read();
   accelZ = Wire.read() << 8 | Wire.read();
   Wire.beginTransmission(MPU);
   Wire.write(0x43);
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU, 6,true);
+  Wire.requestFrom(MPU, 6, true);
   gyroX = Wire.read() << 8 | Wire.read();
   gyroY = Wire.read() << 8 | Wire.read();
   gyroZ = Wire.read() << 8 | Wire.read();
@@ -68,17 +100,19 @@ float baseAcX, baseAcY, baseAcZ;
 float baseGyX, baseGyY, baseGyZ;
 void getInitavg()
 {
-  float sumAcX=0, sumAcY=0, sumAcZ=0;
-  float sumGyX=0, sumGyY=0, sumGyZ=0;
+  float sumAcX = 0, sumAcY = 0, sumAcZ = 0;
+  float sumGyX = 0, sumGyY = 0, sumGyZ = 0;
   for (int i = 0; i < 10; i++)
   {
     getAngle();
     sumAcX += accelX; sumAcY += accelY; sumAcZ += accelZ;
     sumGyX += gyroX; sumGyY += gyroY; sumGyZ += gyroZ;
+    delay(100);
   }
   baseAcX = sumAcX / 10;    baseAcY = sumAcY / 10;    baseAcZ = sumAcZ / 10;
   baseGyX = sumGyX / 10;   baseGyY = sumGyY / 10;   baseGyZ = sumGyZ / 10;
 }
+unsigned long Tnow, Tprev;
 void InitDt()
 {
   Tprev = micros();
@@ -121,6 +155,7 @@ void calGyro()
   finalGyroX += gyro_x*dT;
   finalGyroY += gyro_y*dT;
   finalGyroZ += gyro_z*dT;
+  
 }
 float complememtX, complememtY, complememtZ;
 void complementary()
@@ -136,7 +171,7 @@ void complementary()
 float base_roll_target_angle;
 float base_pitch_target_angle;
 float base_yaw_target_angle;
-float roll_target_angle=0.0, pitch_target_angle=0.0, yaw_target_angle=0.0;
+ extern float roll_target_angle , pitch_target_angle , yaw_target_angle ;
 void  InitYPR()
 {
   for (int i = 0; i < 10; i++)
@@ -159,7 +194,7 @@ void  InitYPR()
   yaw_target_angle = base_yaw_target_angle;
 }
 
-void calcYPTtoStdPID()
+/*void calcYPTtoStdPID()
 {
   stdPID(roll_target_angle, complememtY, roll_prev_angle, roll_kp, roll_ki, roll_kd, roll_iterm, roll_output);
   stdPID(pitch_target_angle, complememtX, pitch_prev_angle, pitch_kp, pitch_ki, pitch_kd, pitch_iterm, pitch_output);
@@ -177,15 +212,15 @@ void stdPID(float& setpoint, float& input, float& prev_input, float& kp, float& 
   iterm += ki*error*dT;
   dterm = -kd*dInput / dT;
   output = pterm + iterm + dterm;
-}
-float throttle =0;
+}*/
+float throttle = 0;
 float motorA_speed, motorB_speed, motorC_speed, motorD_speed;
 void calcMotorSpeed()
 {
-  motorA_speed =(throttle==0)?0: throttle + yaw_output + roll_output + pitch_output;
-  motorB_speed = (throttle==0)?0:throttle - yaw_output - roll_output + pitch_output;
-  motorC_speed =(throttle==0) ?0: throttle + yaw_output - roll_output - pitch_output;
-  motorD_speed = (throttle==0)?0: throttle - yaw_output + roll_output - pitch_output;
+  motorA_speed = (throttle == 0) ? 0 : throttle + yaw_output + roll_output + pitch_output;
+  motorB_speed = (throttle == 0) ? 0 : throttle - yaw_output - roll_output + pitch_output;
+  motorC_speed = (throttle == 0) ? 0 : throttle + yaw_output - roll_output - pitch_output;
+  motorD_speed = (throttle == 0) ? 0 : throttle - yaw_output + roll_output - pitch_output;
   if (motorA_speed < 0)
     motorA_speed = 0;
   if (motorA_speed>255)
@@ -203,49 +238,49 @@ void calcMotorSpeed()
   if (motorD_speed > 255)
     motorD_speed = 255;
 }
-void SendDataToProcessing()
+/*void SendDataToProcessing()
 {
-  Serial.print(F("DEL:"));
-  Serial.print(dT, DEC);
-  Serial.print(F("#RPY:"));
-  Serial.print(complememtX, 2);
-  Serial.print(F(", "));
-  Serial.print(complememtY, 2);
-  Serial.print(F(", "));
-  Serial.print(complememtZ, 2);
-  Serial.print(F("finalGyroZ"));
-  Serial.print(finalGyroZ, 2);
-  Serial.print(F("#PID:"));
-  Serial.print(roll_output, 2);
-  Serial.print(F(", "));
-  Serial.print(pitch_output, 2);
-  Serial.print(F(", "));
-  Serial.print(yaw_output, 2);
-  Serial.print(F("#A:"));
-  Serial.print(motorA_speed, 2);
-  Serial.print(F("#B: "));
-  Serial.print(motorB_speed, 2);
-  Serial.print(F("#C: "));
-  Serial.print(motorC_speed, 2);
-  Serial.print(F("#D: "));
-  Serial.println(motorD_speed, 2);
-  delay(100);
-}
+Serial.print(F("DEL:"));
+Serial.print(dT, DEC);
+Serial.print(F("#RPY:"));
+Serial.print(complememtX, 2);
+Serial.print(F(", "));
+Serial.print(complememtY, 2);
+Serial.print(F(", "));
+Serial.print(complememtZ, 2);
+Serial.print(F("finalGyroZ"));
+Serial.print(finalGyroZ, 2);
+Serial.print(F("#PID:"));
+Serial.print(roll_output, 2);
+Serial.print(F(", "));
+Serial.print(pitch_output, 2);
+Serial.print(F(", "));
+Serial.print(yaw_output, 2);
+Serial.print(F("#A:"));
+Serial.print(motorA_speed, 2);
+Serial.print(F("#B: "));
+Serial.print(motorB_speed, 2);
+Serial.print(F("#C: "));
+Serial.print(motorC_speed, 2);
+Serial.print(F("#D: "));
+Serial.println(motorD_speed, 2);
+delay(100);
+}*/
 uint8_t mspPacket[11];
 void checkMspPacket()
 {
   static uint32_t cnt;
-  
+
   if (Serial1.available()>0)
   {
     while (Serial1.available()>0)
     {
-      uint8_t inputdata= Serial1.read();
+      uint8_t inputdata = Serial1.read();
       if (inputdata == '$')
-        cnt=head1;
+        cnt = head1;
       else cnt++;
       mspPacket[cnt] = inputdata;
-      if(cnt == crc)
+      if (cnt == crc)
       {
         if (mspPacket[type] == 150)
         {
@@ -255,22 +290,58 @@ void checkMspPacket()
     }
   }
 }
-int motorA =6;
+int motorA = 6;
 int motorB = 10;
-int motorC= 9;
+int motorC = 9;
 int motorD = 5;
 void InitSpeed()
 {
-  analogWrite(motorA,0);
-  analogWrite( motorB,0);
-  analogWrite(motorC,0);
-  analogWrite(motorD,0);
+  analogWrite(motorA, 0);
+  analogWrite(motorB, 0);
+  analogWrite(motorC, 0);
+  analogWrite(motorD, 0);
 }
 void updateSpeed()
 {
-  analogWrite(motorA,motorA_speed);
-  analogWrite( motorB,motorB_speed);
-  analogWrite(motorC,motorC_speed);
-  analogWrite(motorD,motorD_speed);
+  analogWrite(motorA, motorA_speed);
+  analogWrite(motorB, motorB_speed);
+  analogWrite(motorC, motorC_speed);
+  analogWrite(motorD, motorD_speed);
+}
+void DualPID(float target_angle,
+  float angle_in,
+  float rate_in,
+  float stabilize_kp,
+  float stabilize_ki,
+  float rate_kp,
+  float rate_ki,
+  float &stabilize_iterm, float &rate_iterm, float&output)
+{
+  float angle_error;
+  float desired_rate;
+  float rate_error;
+  float stabilize_pterm, rate_pterm;
+  angle_error = target_angle - angle_in;
+  stabilize_pterm = stabilize_kp *angle_error;
+  stabilize_iterm += stabilize_ki*angle_error*dT;
+  desired_rate = stabilize_pterm;
+  rate_error = desired_rate - rate_in;
+  rate_pterm = rate_kp*rate_error;
+  rate_iterm += rate_ki*rate_error*dT;
+  output = rate_pterm + rate_iterm + stabilize_iterm;
 }
 
+void calcYPRtoDualPID()
+{
+  roll_angle_in = complememtY;
+  roll_rate_in = gyro_y;
+  DualPID(roll_target_angle, roll_angle_in, roll_rate_in, roll_stabilize_kp, roll_stabilize_ki, roll_rate_kp, roll_rate_ki, roll_stabilize_iterm, roll_rate_itrem, roll_output);
+  pitch_angle_in = complememtX;
+  pitch_rate_in = gyro_x;
+  DualPID(pitch_target_angle, pitch_angle_in, pitch_rate_in, pitch_stabilize_kp, pitch_stabilize_ki, pitch_rate_kp, pitch_rate_ki, pitch_stabilize_iterm, pitch_rate_itrem, pitch_output);
+  yaw_angle_in = complememtZ;
+  yaw_rate_in = gyro_z;
+  DualPID(yaw_target_angle, yaw_angle_in, yaw_rate_in,yaw_stabilize_kp, yaw_stabilize_ki, yaw_rate_kp, yaw_rate_ki, yaw_stabilize_iterm, yaw_rate_itrem, yaw_output);
+
+
+}
